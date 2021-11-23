4980f2d8caf310b5c632adea566b2b5b093b2ca5
Lance Hebert <LSUHEBERT@gmail.com>
2021-11-22 23:24:09 -0800

    commit tree 9b792781387200417f4ac7801cd4c47ceae145d0

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
-function saturdayFun(activity="roller-skate") {
-  return `This Saturday, I want to ${activity}!`
+function saturdayFun(input = "roller-skate")
+{
+  return `This Saturday, I want to ${input}!`;  
 }
 
-let mondayWork = function(activity="go to the office") {
-  return `This Monday, I will ${activity}.`
+function mondayWork(input = "go to the office")
+{
+  return `This Monday, I will ${input}.`
 }
 
-let wrapAdjective = function(style="*") {
-  return function(adjective="special") {
-    return `You are ${style}${adjective}${style}!`
+function wrapAdjective(flair = "*")
+{
+  const innerFunction = function(innerInput = "special")
+  {
+    return `You are ${flair}${innerInput}${flair}!`;
   }
+  return innerFunction;
 }
